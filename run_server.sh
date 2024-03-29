052b4a4c19c3ec657063f3eaab596ac6b3aa9a96
wgh-server <3030649311@qq.com>
2024-03-29 11:31:23 +0800

    updated run.sh

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,14 @@
-git clone https://github.com/wgh-z/algorithm_server.git
-cd algorithm_server
+#!/bin/bash
 
-docker build --pull --rm -f "Dockerfile.algorithm_server" -t algorithm_server:2.2.0-cuda12.1-cudnn8-runtime "."
+# chmod +x run_server.sh
 
-docker run -d --gpus all --network=host --name a_server algorithm_server:2.2.0-cuda12.1-cudnn8-runtime
+# git clone https://github.com/wgh-z/algorithm_server.git
+# cd algorithm_server
 
-# docker run --rm -it --gpus all -p 8554:8554 -p 4006:4006 --name a_server algorithm_server:2.2.0-cuda12.1-cudnn8-runtime
+# docker build --pull --rm -f "Dockerfile.algorithm_server" -t algorithm_server:2.2.0-cuda12.1-cudnn8-runtime "."
+
+# docker run -d --gpus all --network=host --name a_server algorithm_server:2.2.0-cuda12.1-cudnn8-runtime
+
+PORT=${1:-8554}
+
+docker run --rm -it --gpus all -p $PORT:8554 -p 4006:4006 --name a_server algorithm_server:2.2.0-cuda12.1-cudnn8-runtime
